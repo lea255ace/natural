27925b7f1d71522035c5b1f35c8f6fd76e34c3d6
Mick Wollman <mick@bit-bar.org>
2023-05-19 09:29:39 -0400

    Initial commit.

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Natural Time</title>
+      </Head>
+      <h1>Natural Time!</h1>
+    </>
+  );
+}
