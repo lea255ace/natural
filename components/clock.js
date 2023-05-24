0392b5974bb93f617ce8b90cecfbe5ab0d5786de
Mick Wollman <mick@bit-bar.org>
2023-05-24 09:58:52 -0400

    Add moving parts to the Clock component.

--- components/clock.js
+++ components/clock.js
@@ -3,11 +3,15 @@ import styles from './clock.module.css';
 
 const MINUTES_PER_DAY = 1440;
 
+//TODO(MW): Math.floor could be introducing some imprecision, should these use sig figs instead?
 export default function Clock({ civilTimeMinutes, civilTimeOffsetMinutes, currentDaylightMinutes, maxDaylightMinutes }) {
+    const civilTimeAngle = Math.floor(((civilTimeMinutes + civilTimeOffsetMinutes) * 360) / MINUTES_PER_DAY);
     const civilTimeOffsetAngle = Math.floor((civilTimeOffsetMinutes * 360) / MINUTES_PER_DAY);
-    var civilClockStyle = {
-        rotate: civilTimeOffsetAngle + 'deg',
-    };
+
+    const solsticeAngle = Math.floor(((MINUTES_PER_DAY - 2 * maxDaylightMinutes) * 90) / MINUTES_PER_DAY);
+    const sunAngle = Math.floor(((MINUTES_PER_DAY - 2 * currentDaylightMinutes) * 90) / MINUTES_PER_DAY);
+    const middayAngle = Math.floor(sunAngle / 2) + 45;
+    const midnightAngle = Math.floor(sunAngle / 2) - 45;
 
     return (
         <div>
@@ -19,12 +23,78 @@ export default function Clock({ civilTimeMinutes, civilTimeOffsetMinutes, curren
                     src='/static/Civil Clock.png'
                     className={styles.civil}
                     fill='true'
-                    style={civilClockStyle}
+                    style={{ rotate: civilTimeOffsetAngle + 'deg' }}
                 />
                 <Image
                     src='/static/Solar Clock.png'
                     className={styles.solar}
                     fill='true'
+                />
+                <Image
+                    src='/static/Solstice Line.png'
+                    className={styles.solstice}
+                    fill='true'
+                    style={{ rotate: (solsticeAngle) + 'deg' }}
+                />
+                <Image
+                    src='/static/Solstice Line.png'
+                    className={styles.solstice}
+                    fill='true'
+                    style={{ rotate: (-solsticeAngle) + 'deg' }}
+                />
+                <Image
+                    src='/static/Solstice Line.png'
+                    className={styles.solstice}
+                    fill='true'
+                    style={{ rotate: (180 + solsticeAngle) + 'deg' }}
+                />
+                <Image
+                    src='/static/Solstice Line.png'
+                    className={styles.solstice}
+                    fill='true'
+                    style={{ rotate: (180 - solsticeAngle) + 'deg' }}
+                />
+                <Image
+                    src='/static/Sun Line.png'
+                    className={styles.sun}
+                    fill='true'
+                    style={{ rotate: (sunAngle) + 'deg' }}
+                />
+                <Image
+                    src='/static/Sun Line.png'
+                    className={styles.sun}
+                    fill='true'
+                    style={{ rotate: (180 - sunAngle) + 'deg' }}
+                />
+                <Image
+                    src='/static/Mid Line.png'
+                    className={styles.mid}
+                    fill='true'
+                    style={{ rotate: (middayAngle) + 'deg' }}
+                />
+                <Image
+                    src='/static/Mid Line.png'
+                    className={styles.mid}
+                    fill='true'
+                    style={{ rotate: (180 - middayAngle) + 'deg' }}
+                />
+                <Image
+                    src='/static/Mid Line.png'
+                    className={styles.mid}
+                    fill='true'
+                    style={{ rotate: (midnightAngle) + 'deg' }}
+                />
+                <Image
+                    src='/static/Mid Line.png'
+                    className={styles.mid}
+                    fill='true'
+                    style={{ rotate: (180 - midnightAngle) + 'deg' }}
+                />
+                <Image
+                    src='/static/Time Hand.png'
+                    className={styles.hand}
+                    fill='true'
+                    style={{ rotate: civilTimeAngle + 'deg' }}
                 />
             </div>
         </div>
