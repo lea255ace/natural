8232b0d8db7d882334915a135eadee48f2562c03
Mick Wollman <mick@bit-bar.org>
2023-05-24 11:08:38 -0400

    Provide the current time to the Clock component and check for a change in minutes once per second.

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,22 @@
 import Head from 'next/head';
 import Clock from '../components/clock'
 import { useConfigContext } from "../context/configProvider";
+import { useState, useEffect } from 'react';
 
 export default function Home() {
+  let initialDate = new Date();
+  const initialMinutes = (initialDate.getHours() * 60) + initialDate.getMinutes();
   const [configValues, updateConfigValues] = useConfigContext();
+  const [currentTimeMinutes, setCurrentTimeMinutes] = useState(initialMinutes);
+
+  useEffect(() => {
+    const tick = setInterval(() => {
+      let newDate = new Date();
+      let newTimeMinutes = (newDate.getHours() * 60) + newDate.getMinutes();
+      setCurrentTimeMinutes(newTimeMinutes);
+    }, 1000);
+    return () => clearInterval(tick);
+  }, []);
 
   return (
     <>
@@ -14,7 +27,7 @@ export default function Home() {
       <p>Current Latitude: {Math.abs(configValues.latitude) + " " + ((configValues.latitude > 0) ? "N" : "S")}</p>
       <p>Current Longitude: {Math.abs(configValues.longitude) + " " + ((configValues.longitude > 0) ? "E" : "W")}</p>
       <Clock
-        civilTimeMinutes={328}
+        civilTimeMinutes={currentTimeMinutes}
         civilTimeOffsetMinutes={41.23}
         currentDaylightMinutes={805}
         maxDaylightMinutes={867}
