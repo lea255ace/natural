781d9b43a6c3a380ce9c5bc42ca8fc66f4378d91
Mick Wollman <mick@bit-bar.org>
2023-05-25 16:25:33 -0400

    Calculate the current daylight minutes based on the date.

--- pages/index.js
+++ pages/index.js
@@ -9,6 +9,43 @@ export default function Home() {
   const [configValues, updateConfigValues] = useConfigContext();
   const [currentTimeMinutes, setCurrentTimeMinutes] = useState(initialMinutes);
 
+  //TODO(MW): This should be hoisted into a utility module
+  // Calculations provided at https://gml.noaa.gov/grad/solcalc/solareqns.pdf
+  const fractionalYear = calculateFractionalYear(initialDate);
+  const declination = calculateDeclination(fractionalYear);
+  const eqTime = calculateEqTime(fractionalYear);
+  const sunriseHourAngleDegrees = calculateSunriseHourAngleDegrees(configValues.latitude, declination);
+  const sunriseTimeMinutes = 720 - 4 * (configValues.longitude + sunriseHourAngleDegrees) - eqTime;
+  const sunsetTimeMinutes = 720 - 4 * (configValues.longitude - sunriseHourAngleDegrees) - eqTime;
+  const daylightMinutes = sunsetTimeMinutes - sunriseTimeMinutes;
+
+  function calculateFractionalYear(date) {
+    const dayOfYear = Math.floor((date - new Date(date.getFullYear(), 0, 0)) / 1000 / 60 / 60 / 24);
+    const hour = date.getHours() + (date.getMinutes() / 60);
+
+    return (2 * Math.PI) * (dayOfYear - 1 + (hour - 12)/24) / 365;
+  }
+
+  function calculateEqTime(fractionalYear) {
+    return 0.006918 - 0.399912 * Math.cos(fractionalYear) + 0.070257 * Math.sin(fractionalYear)
+                    - 0.006758 * Math.cos(2 * fractionalYear) + 0.000907 * Math.sin(2 * fractionalYear)
+                    - 0.002697 * Math.cos(3 * fractionalYear) + 0.00148 * Math.sin (3 * fractionalYear);
+  }
+
+  function calculateDeclination(fractionalYear) {
+    return 0.006918 - 0.399912 * Math.cos(fractionalYear) + 0.070257 * Math.sin(fractionalYear)
+                    - 0.006758 * Math.cos(2 * fractionalYear) + 0.000907 * Math.sin(2 * fractionalYear)
+                    - 0.002697 * Math.cos(3 * fractionalYear) + 0.00148 * Math.sin (3 * fractionalYear);
+
+  }
+
+  function calculateSunriseHourAngleDegrees(latitude, declination) {
+    const solarZenithRadians = 90.833 * 2 * Math.PI / 360;
+    const latitudeRadians = latitude * 2 * Math.PI / 360;
+    const hourAngleRadians =  Math.acos( Math.cos(solarZenithRadians) / (Math.cos(latitudeRadians) * Math.cos(declination)) - Math.tan(latitudeRadians) * Math.tan(declination) );
+    return hourAngleRadians * 360 / (2 * Math.PI);
+  }
+
   useEffect(() => {
     const tick = setInterval(() => {
       let newDate = new Date();
@@ -29,7 +66,7 @@ export default function Home() {
       <Clock
         civilTimeMinutes={currentTimeMinutes}
         civilTimeOffsetMinutes={41.23}
-        currentDaylightMinutes={805}
+        currentDaylightMinutes={daylightMinutes}
         maxDaylightMinutes={867}
       />
     </>
