cca299624856160efafd9a63d1adad82b57fb96b
Mick Wollman <mick@bit-bar.org>
2023-06-20 12:37:36 -0400

    Update configuration to support debugging the exported site.

--- next.config.js
+++ next.config.js
@@ -3,10 +3,12 @@
 //TODO(MW): This could be moved into .env.production and .env.development?
 //          As per: https://nextjs.org/docs/app/building-your-application/configuring/environment-variables
 const isProd = process.env.NODE_ENV === 'production';
+const debugBuild = process.env.BUILD_MODE === 'debug';
+const prefix = (isProd & !debugBuild) ? '/natural_time' : '';
 
 const nextConfig = {
     output: 'export',
-    assetPrefix: isProd ? '/natural_time' : '',
+    assetPrefix: prefix,
     images: {
         unoptimized: true,
     }
