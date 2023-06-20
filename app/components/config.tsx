52aefdabb7d9059cc7ca75f2fec6c3420d304399
Mick Wollman <mick@bit-bar.org>
2023-06-20 12:37:56 -0400

    Fix a bug with how the config inputs were being updated.

--- app/components/config.tsx
+++ app/components/config.tsx
@@ -1,28 +1,49 @@
+'use client';
+
+import { useState, useEffect } from 'react';
 import { ConfigParameters, useConfigContext } from 'context/configProvider';
+
+type DisplayedParameters = {
+    latitude: string;
+    longitude: string;
+};
 
 export default function Config({ className }) {
     const {configValues, updateConfig} = useConfigContext();
+    const [displayedValues, setDisplayedValues] = useState<DisplayedParameters>(translateConfigValues(configValues));
 
-    function updateValues(e) {
+    function translateConfigValues(configValues: ConfigParameters) {
+        return { latitude: configValues.latitude.toString(), longitude: configValues.longitude.toString() };
+    }
+
+    function updateConfigValues(e) {
         e.preventDefault();
         const form = e.target;
         const formData = new FormData(form);
         const parameters = {} as ConfigParameters;
         for (const [k, v] of formData.entries()) {
-            parameters[k] = v;
+            parameters[k] = parseFloat(v as string);
         }
         updateConfig(parameters);
     }
 
+    useEffect(() => {
+        setDisplayedValues(translateConfigValues(configValues));
+    }, [configValues]);
+
     return (
         <div className={`${className}`}>
-            <form onSubmit={updateValues}>
-                <label>
-                    Latitude: <input name='latitude' defaultValue={configValues.latitude} />
+            <form onSubmit={updateConfigValues}>
+                <label>Latitude:
+                    <input name='latitude' value={displayedValues.latitude}
+                        onChange={e => setDisplayedValues({...displayedValues, latitude: e.target.value})}
+                    />
                 </label>
                 <br />
-                <label>
-                    Longitude: <input name='longitude' defaultValue={configValues.longitude} />
+                <label>Longitude:
+                    <input name='longitude' value={displayedValues.longitude}
+                        onChange={e => setDisplayedValues({...displayedValues, longitude: e.target.value})}
+                    />
                 </label>
                 <br />
                 <button type='reset'>Reset</button>
