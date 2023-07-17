f1587d42588ccf3c67830b66846d7fa811a19faa
Mick Wollman <mick@bit-bar.org>
2023-07-17 14:32:51 -0400

    Move MDX dependencies out of dev to fix deployment builds.

--- next.config.mjs
+++ next.config.mjs
@@ -4,12 +4,14 @@ import nextMdx from '@next/mdx';
 //          As per: https://nextjs.org/docs/app/building-your-application/configuring/environment-variables
 const isProd = process.env.NODE_ENV === 'production';
 const debugBuild = process.env.BUILD_MODE === 'debug';
+const basePath = (isProd & !debugBuild) ? '/natural_time' : '';
 const prefix = (isProd & !debugBuild) ? '/natural_time' : '';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
     assetPrefix: prefix,
+    basePath: basePath,
     images: {
         unoptimized: true,
     }
@@ -17,7 +19,8 @@ const nextConfig = {
 
 const withMDX = nextMdx({
     options: {
-        remarkPlugins: []
+        remarkPlugins: [],
+        target: 'serverless'
     }
 });
 
