058d0a1e6d9c1aa48a98c9a02e93830dcb38f64d
Mick Wollman <mick@bit-bar.org>
2023-07-17 13:48:05 -0400

    Port deleted README content into the /about page.

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -2,8 +2,11 @@
 
 // The MDX pageExtensions feature doesn't support the 'use client' directive, so we have to import the content
 // into a client component skeleton page in order to get Chakra to function properly.
-import About from './about.mdx';
+import { Box } from '@chakra-ui/react';
+import About from 'about/about.mdx';
 
 export default function Page() {
-    return <About />;
+    return <Box maxWidth='800px' mx='auto' px='8' py='2'>
+        <About />
+    </Box>;
 }
