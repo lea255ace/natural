51745e480f7237a1610596ec632eba6e5234b4e5
Mick Wollman <mick@bit-bar.org>
2023-07-21 13:27:29 -0400

    Add the About page.

--- app/components/annotation.tsx
+++ app/components/annotation.tsx
@@ -8,14 +8,15 @@ import {
     Portal,
 } from '@chakra-ui/react';
 
-export default function Annotation ({ trigger, body }: { trigger: string, body: string }) {
+export default function Annotation ({ trigger, body }) {
     return (
-        <Popover placement='right-end' offset={[0, 6]}>
+        <Popover placement='right-end' offset={[0, 6]}
+            modifiers={[{ name: 'flip', options: { fallbackPlacements: ['bottom-end'] } }]}>
             <PopoverTrigger>
                 <chakra.span textColor='purple.500'><sup>[<u>{trigger}</u>]</sup></chakra.span>
             </PopoverTrigger>
             <Portal>
-                <PopoverContent width='auto' maxWidth='200px' p='0'
+                <PopoverContent width='auto' maxWidth='350px' p='0'
                     bg='yellow.50' borderRadius='4' borderColor='gray.400'
                     boxShadow='md' _focus={{boxShadow: 'md'}}>
                     <PopoverBody fontSize='sm'>
--- mdx-components.tsx
+++ mdx-components.tsx
@@ -4,7 +4,7 @@ import { Link } from '@chakra-ui/next-js';
 
 const customComponents = {
     h1: ({children}) => <Heading as="h1" size='2xl'>{children}</Heading>,
-    h2: ({children}) => <Heading as="h2" size='xl'>{children}</Heading>,
+    h2: ({children}) => <Heading as="h2" size='lg'>{children}</Heading>,
     p: ({children}) => <Text marginBottom='4'>{children}</Text>,
     a: ({href, children}) => <Link href={href} >{children}</Link>,
 };
