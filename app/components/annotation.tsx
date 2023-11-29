3efab57eb3f8b3688ed3303a4cd498f16f1d75bb
Mick Wollman <mick@bit-bar.org>
2023-11-29 12:51:49 -0500

    Update annotation component to change mouse to pointer on rollover.

--- app/components/annotation.tsx
+++ app/components/annotation.tsx
@@ -13,12 +13,12 @@ export default function Annotation ({ trigger, body }) {
         <Popover placement='right-end' offset={[0, 6]}
             modifiers={[{ name: 'flip', options: { fallbackPlacements: ['bottom-end'] } }]}>
             <PopoverTrigger>
-                <chakra.span textColor='purple.500'><sup>[<u>{trigger}</u>]</sup></chakra.span>
+                <chakra.span textColor='purple.500' cursor='pointer'><sup>[<u>{trigger}</u>]</sup></chakra.span>
             </PopoverTrigger>
             <Portal>
                 <PopoverContent width='auto' maxWidth='350px' p='0'
                     bg='yellow.50' borderRadius='4' borderColor='gray.400'
-                    boxShadow='md' _focus={{boxShadow: 'md'}}>
+                    boxShadow='md'>
                     <PopoverBody fontSize='sm'>
                         {body}
                     </PopoverBody>
